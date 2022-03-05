e8547f994d16198b1a937d79de9ceff5a019fef4
Bimal Raj Gyawali <gyawali.rajbimal35@gmail.com>
2022-03-05 22:34:21 +0545

    tests repo methods

--- src/main/java/me/personal/HibernateApplication.java
+++ src/main/java/me/personal/HibernateApplication.java
@@ -1,10 +1,27 @@
 package me.personal;
 
-import me.personal.config.HibernateConfig;
+import me.personal.entity.Customer;
+import me.personal.repo.CustomerRepo;
+import me.personal.repo.CustomerRepoImpl;
 
 public class HibernateApplication {
     public static void main(String[] args) {
         System.out.println("Hello Hibernate");
-        System.out.println(HibernateConfig.getSessionFactory());
+
+        CustomerRepo customerRepo = new CustomerRepoImpl();
+
+        var savedCustomerId = customerRepo.save(
+                new Customer()
+                        .fullName("Ram")
+                        .address("Lalitpur")
+        );
+
+        System.out.println(savedCustomerId);
+
+        System.out.println(customerRepo.findById(11L));
+
+        System.out.println(customerRepo.findByFullName("John"));
+
+        System.out.println(customerRepo.findByAddress("KTM"));
     }
 }
